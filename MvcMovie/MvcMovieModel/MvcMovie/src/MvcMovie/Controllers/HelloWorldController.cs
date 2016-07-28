f94ad987a72f2c07ff3a4c2c92e68124575b372d
seanlehman <sjlehman4@comcast.net>
2016-07-28 15:15:55 -0400

    Added a view

--- MvcMovie/MvcMovieModel/MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovieModel/MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -12,16 +12,19 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome/
 
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes: {numTimes}");
+            ViewData["Message"] = "Hello! " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
         }
     }
 }
