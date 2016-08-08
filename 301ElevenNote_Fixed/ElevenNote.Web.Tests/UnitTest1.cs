489947c8df74a54e92c1771f69162b84edc0d96b
seanlehman <sjlehman4@comcast.net>
2016-08-07 23:38:13 -0400

    Auri adde code

---
+++ 301ElevenNote_Fixed/ElevenNote.Web.Tests/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using ElevenNote.Web.Controllers;
+using ElevenNote.Web.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElevenNote.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static bool ValidateObject(object o)
+        {
+            var results = new List<ValidationResult>();
+
+            try
+            {
+                Validator.TryValidateObject(o, new ValidationContext(o), results, true /* validate all properties */);
+                return results.Count == 0;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        [TestMethod]
+        public void IndexPageViewBagShouldSetTitleToHome()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Home", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void UsernameRequiredByModelToRegister()
+        {
+            // Arrange.
+            var model = new RegisterViewModel();
+            model.Email = null;
+            model.ConfirmPassword = "LetMeIn1234!";
+            model.Password = "LetMeIn1234!";
+
+            //Act.
+            var result = ValidateObject(model);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        
+        public void PasswordsMustMatchToRegister()
+        {
+            //Arrange
+            var model = new RegisterViewModel
+            {
+                Email = "test@test.com",
+                ConfirmPassword = "LetMeIn1234!",
+                Password = "LeIn1234!"
+                 
+            };
+
+            //Act
+            var result = ValidateObject(model);
+
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
