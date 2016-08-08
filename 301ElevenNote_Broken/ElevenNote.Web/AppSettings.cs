9196dc62ca4c7a9897fcb90bda16e0d4a9c1bb08
seanlehman <sjlehman4@comcast.net>
2016-08-07 23:45:06 -0400

    Fixed Auri's Code

---
+++ 301ElevenNote_Broken/ElevenNote.Web/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web
+{
+    public static class AppSettings
+    {
+        public static bool LoginEnabled = bool.Parse(ConfigurationManager.AppSettings["Site.Authentication.LoginEnabled"]);
+
+        public static int PasswordRequiredLength =
+            int.Parse(ConfigurationManager.AppSettings["Site.Authentication.PasswordSettings.RequiredLength"]);
+
+        public static bool PasswordRequireNonLetterOrDigit = 
+            bool.Parse(ConfigurationManager.AppSettings["Site.Authentication.PasswordSettings.RequireNonLetterOrDigit"]);
+
+        public static bool PasswordRequireDigit = 
+            bool.Parse(ConfigurationManager.AppSettings["Site.Authentication.PasswordSettings.RequireDigit"]);
+
+        public static bool PasswordRequireLowercase = 
+            bool.Parse(ConfigurationManager.AppSettings["Site.Authentication.PasswordSettings.RequireLowercase"]);
+
+        public static bool PasswordRequireUppercase = 
+            bool.Parse(ConfigurationManager.AppSettings["Site.Authentication.PasswordSettings.RequireUppercase"]);
+    }
+}
