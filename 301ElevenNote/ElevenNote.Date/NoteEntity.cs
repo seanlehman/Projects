0eb6acead667b3465e8f2719b378077933ad3170
seanlehman <sjlehman4@comcast.net>
2016-09-07 16:17:08 -0400

    test

---
+++ 301ElevenNote/ElevenNote.Date/NoteEntity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Date
+{
+    [Table("Note")]
+
+    public class NoteEntity
+    {
+        [Key] /*Makes the NoteId unique so you don't have to have #1, #2, etc*/
+
+        public int NoteId { get; set; }
+
+        [Required]
+
+        public Guid OwnerId { get; set; }
+
+        [Required]
+
+        public string Title { get; set; }
+
+        [Required]
+
+        public string Content { get; set; }
+
+        [DefaultValue(false)]
+
+        public bool IsStarred { get; set; }
+
+        [Required]
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; } /*? means not filled in by default*/
+
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+    }
+}
---
+++ 301ElevenNote/ElevenNote.Models/NoteEditModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteEditModel
+    {
+        public int NoteId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsStarred { get; set; }// Get data from database by using NoteService
+
+        public override string ToString()
+        {
+            return $"[{NoteId}] {Title}";
+        }
+    }
+}
