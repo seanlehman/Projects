a2dc27548740d50ba60456dc998f252c4a8ef76c
seanlehman <sjlehman4@comcast.net>
2016-09-23 14:51:36 -0400

    Added File and FileType Models

---
+++ ShoppingList_N1/ShoppingListApp/Models/File.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ShoppingListApp.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        [StringLength(100)]
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+        public FileType FileType { get; set; }
+        public int PersonId { get; set; }
+        public virtual Person Person { get; set; }
+    }
+}
+
