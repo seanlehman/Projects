bf4f2b53896ddf1aa5f0ca9f9a5759c9005df253
seanlehman <sjlehman4@comcast.net>
2016-09-23 14:22:18 -0400

    Added FilePath Model

---
+++ ShoppingList_N1/ShoppingListApp/Models/FilePath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingListApp.Models
+{
+    public class FilePath
+    {
+    }
+}
+
+
+namespace ShoppingListApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class FilePath
+    {
+        public int FilePathId { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        public FileType FileType { get; set; }
+        public int PersonID { get; set; }
+        public virtual Person Person { get; set; }
+    }
+}
